0486e981bd3934562d4af6f17f1509b618ac74ba
tofuadmiral <fuad98ali@gmail.com>
2018-09-22 20:39:51 -0400

    finished lab w help from daniel, writing report

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -35,13 +35,61 @@ title('Ahmed Fuad Ali 400075937 & Daniel Jabbour 400095100');
 %     vector, and vector of column indices, generate vector of avg grades, 
 %     one for each, associated w the right column index
 
-% read the grades from the CSV file
-m = csvread('course_grades_2018.csv', 1, 1, [1, 1, 20, 11]);
-max = csvread('course_grades_2018.csv', 0, 1, [0, 1, 0, 11]); 
-% third param is a vector of column indices
-r = [2:5];
-% call the function csv_get_averages.m to get one for each assignment 
-grades = csv_get_averages(m, max,r);
+%% Question 2
+
+marks = csvread('course_grades_2018.csv', 1, 0);
+
+max_marks = csvread('course_grades_2018.csv', 0, 1, [0, 1, 0, 11]);
+
+% Extracting the appropriate data from the matrix
+exam_col = (6:11);
+midterm_col = (5:5);
+labs_col = (1:4);
+
+% b)
+
+% Calculating the averages for each respective mark
+exam_avg = csv_get_averages(marks, max_marks, exam_col);
+
+midterm_avg = csv_get_averages(marks, max_marks, midterm_col);
+
+labs_avg = csv_get_averages(marks, max_marks, labs_col);
+
+% c)
+
+cumulitive_avg = 0.4.*(exam_avg) + 0.3.*(midterm_avg) + 0.3.*(labs_avg);
+
+cumulitive_avg_desc = sort(transpose(cumulitive_avg), 'descend');
+
+plot(cumulitive_avg_desc)
+title('Ahmed Fuad Ali 400075937 & Daniel Jabbour 400095100');
+
+% Question 3
+
+image = imread('ee3tp3picture2018.png');
+image_of_doubles = double(image);
+
+% a)
+
+[n_elements, centers] = hist(image_of_doubles(:), 20);
+bar(centers, n_elements)
+xlim([0 255])
+
+% b) 
+
+% c - v'(x,y) = a * v(x, y) + b
+image_of_doubles = image_of_doubles.*9.5 - 1510;
+
+% d)
+[n_elements, centers] = hist(image_of_doubles(:), 20);
+bar(centers, n_elements)
+xlim([0 255])
+title('Ahmed Fuad Ali 400075937 & Daniel Jabbour 400095100')
+
+% e)
+imshow(uint8(image_of_doubles));
+image_to_save = uint8(image_of_doubles);
+imwrite(image_to_save, 'saved_image.png');
 
 
 
